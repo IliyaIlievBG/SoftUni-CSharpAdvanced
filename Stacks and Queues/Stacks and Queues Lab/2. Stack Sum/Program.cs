3570544db252da629bff0a2a71a3effd41a3dfe1
Iliya Iliev <iliya.ilievbg@gmail.com>
2024-01-02 01:06:16 +0200

    Add second problem

---
+++ Stacks and Queues/Stacks and Queues Lab/2. Stack Sum/Program.cs
@@ -0,0 +1,41 @@
+﻿int[] numbers = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+string command = Console.ReadLine().ToLower();
+
+Stack<int> numbersToSum = new Stack<int>(numbers);
+while (command != "end")
+{
+    string[] currentCommand = command.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+    string currentAction = currentCommand[0].ToLower();
+
+    if (currentAction == "add")
+    {
+        numbersToSum.Push(int.Parse(currentCommand[1]));
+        numbersToSum.Push(int.Parse(currentCommand[2]));
+    }
+    else
+    {
+        bool currentCommandIsValid = CheckCurrentCommand(int.Parse(currentCommand[1]), numbersToSum);
+        if (currentCommandIsValid)
+        {
+            for (int i = 0; i < int.Parse(currentCommand[1]); i++)
+            {
+                numbersToSum.Pop();
+            }
+        }
+    }
+
+    command = Console.ReadLine().ToLower();
+}
+
+int sum = 0;
+
+foreach (var num in numbersToSum)
+{
+    sum += num;
+}
+Console.WriteLine(sum);
+static bool CheckCurrentCommand(int numbersToRemove, Stack<int> numbersToSum)
+{
+    return numbersToRemove > numbersToSum.Count ? false : true;
+}
