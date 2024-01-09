c789f653d77b1ed0e60e675f0d07dc506aef8b27
Iliya Iliev <iliya.ilievbg@gmail.com>
2024-01-09 23:57:56 +0200

    Add problems

---
+++ Stacks and Queues/Stacks and Queues Lab/5. Print Even Numbers/Program.cs
@@ -0,0 +1,21 @@
+﻿int[] numbers = Console.ReadLine()
+    .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+    .Select(x => int.Parse(x))
+    .ToArray();
+
+Queue<int> evenNums = new Queue<int>();
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    if (numbers[i] % 2 == 0)
+    {
+        evenNums.Enqueue(numbers[i]);
+    }
+}
+
+Console.Write(evenNums.Dequeue());
+
+while (evenNums.Count>0)
+{
+    Console.Write($", {evenNums.Dequeue()}");
+}
