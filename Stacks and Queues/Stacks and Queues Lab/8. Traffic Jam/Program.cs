72d4f2525a7777fbbf4d5d07b071560bc80bdb5f
Iliya Iliev <iliya.ilievbg@gmail.com>
2024-01-10 23:32:31 +0200

    adding problem Traffic Jam

---
+++ Stacks and Queues/Stacks and Queues Lab/8. Traffic Jam/Program.cs
@@ -0,0 +1,28 @@
+﻿int carsToGoOnGreen = int.Parse(Console.ReadLine());
+
+string car = Console.ReadLine();
+
+Queue<string> carsAtTheTrafficLight = new Queue<string>();
+int passedCars = 0;
+
+while (car != "end")
+{
+    if (car == "green")
+    {
+        for (int i = 0; i < carsToGoOnGreen; i++)
+        {
+            if (carsAtTheTrafficLight.Count > 0)
+            {
+                Console.WriteLine($"{carsAtTheTrafficLight.Dequeue()} passed!");
+                passedCars++;
+            }
+        }
+    }
+    else
+    {
+        carsAtTheTrafficLight.Enqueue(car);
+    }
+    car = Console.ReadLine();
+}
+
+Console.WriteLine($"{passedCars} cars passed the crossroads.");
