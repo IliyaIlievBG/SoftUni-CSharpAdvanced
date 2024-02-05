3a28a275170dbb25cef9927492c933cd90785ea5
Iliya Iliev <iliya.ilievbg@gmail.com>
2024-02-05 23:14:48 +0200

    clean up

---
+++ SetsAndDictionariesAdvanced/SetsAndDictionariesAdvanced-Exercise/1. Unique Usernames/Program.cs
@@ -0,0 +1,13 @@
+﻿HashSet<string> names = new HashSet<string>();
+
+int namesCount = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < namesCount; i++)
+{
+    names.Add(Console.ReadLine());
+}
+
+foreach (var name in names)
+{
+    Console.WriteLine(name);
+}
