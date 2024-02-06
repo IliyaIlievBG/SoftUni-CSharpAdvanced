0d395505d063828fa50a87f7e0712f068af1e32d
Iliya Iliev <iliya.ilievbg@gmail.com>
2024-02-07 00:57:32 +0200

    adding problems
    Adding problems

---
+++ SetsAndDictionariesAdvanced/SetsAndDictionariesAdvanced-Exercise/5.CountSymbol/Program.cs
@@ -0,0 +1,18 @@
+﻿string input = Console.ReadLine();
+
+SortedDictionary<char, int> symbols = new SortedDictionary<char, int>();
+
+for (int i = 0; i < input.Length; i++)
+{
+    if (!symbols.ContainsKey(input[i]))
+    {
+        symbols.Add(input[i], 0);
+    }
+
+    symbols[input[i]]++;
+}
+
+foreach (var (symbol, count) in symbols)
+{
+    Console.WriteLine($"{symbol}: {count} time/s");
+}
---
+++ SetsAndDictionariesAdvanced/SetsAndDictionariesAdvanced-Exercise/6. Wardrobe/Program.cs
@@ -0,0 +1,44 @@
+﻿int inputLines = int.Parse(Console.ReadLine());
+
+Dictionary<string, Dictionary<string, int>> wardrobe = new Dictionary<string, Dictionary<string, int>>();
+
+
+for (int i = 0; i < inputLines; i++)
+{
+    string[] nextSetOfClothes = Console.ReadLine()
+        .Split(new string[] { " -> ", "," }, StringSplitOptions.RemoveEmptyEntries);
+
+    string color = nextSetOfClothes[0];
+
+    if (!wardrobe.ContainsKey(color))
+    {
+        wardrobe.Add(color, new Dictionary<string, int>());
+    }
+
+    for (int j = 1; j < nextSetOfClothes.Length; j++)
+    {
+        if (!wardrobe[color].ContainsKey(nextSetOfClothes[j]))
+        {
+            wardrobe[color].Add(nextSetOfClothes[j], 0);
+        }
+
+        wardrobe[color][nextSetOfClothes[j]]++;
+    }
+
+}
+
+string[] itemToSearch = Console.ReadLine()
+    .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+foreach (var (color, clothes) in wardrobe)
+{
+    Console.WriteLine($"{color} clothes:");
+
+    foreach (var (wear, count) in clothes)
+    {
+        bool foundWeaer = itemToSearch[0] == color && itemToSearch[1] == wear;
+        string foundItem = foundWeaer ? " (found!)" : String.Empty;
+
+        Console.WriteLine($"* {wear} - {count}{foundItem}");
+    }
+}
