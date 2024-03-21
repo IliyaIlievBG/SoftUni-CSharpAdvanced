1e1a4a402e43254e5aeed71e5defa588ca56510e
Iliya Iliev <iliya.ilievbg@gmail.com>
2024-03-22 00:50:55 +0200

    adding problems

---
+++ SetsAndDictionariesAdvanced/SetsAndDictionariesAdvanced-Exercise/10. ForceBook/Program.cs
@@ -0,0 +1,63 @@
+﻿SortedDictionary<string, SortedSet<string>> sides = new SortedDictionary<string, SortedSet<string>>();
+
+string command = Console.ReadLine();
+
+while (command != "Lumpawaroo")
+{
+    string[] currentCommand = command.Split(new string[] { " | ", " -> " }, StringSplitOptions.RemoveEmptyEntries);
+
+    if (command.Contains(" | "))
+    {
+        string user = GetDataFromCurrentCommand(currentCommand, 1);
+        string side = GetDataFromCurrentCommand(currentCommand, 0);
+
+        AddUser(sides, side, user);
+    }
+    else
+    {
+        string user = GetDataFromCurrentCommand(currentCommand, 0);
+        string side = GetDataFromCurrentCommand(currentCommand, 1);
+
+        if (sides.Any(x => x.Value.Contains(user)))
+        {
+            var sideToBeAmmended = sides
+                .Where(x => x.Value.Contains(user))
+                .Select(x => x.Key).Single();
+
+            sides[sideToBeAmmended].Remove(user);
+        }
+
+        AddUser(sides, side, user);
+
+        Console.WriteLine($"{user} joins the {side} side!");
+    }
+
+    command = Console.ReadLine();
+}
+
+foreach (var side in sides.OrderByDescending(x => x.Value.Count))
+{
+    if (side.Value.Count > 0)
+    {
+        Console.WriteLine($"Side: {side.Key}, Members: {side.Value.Count}");
+        foreach (var user in side.Value)
+        {
+            Console.WriteLine($"! {user}");
+        }
+    }
+}
+
+string GetDataFromCurrentCommand(string[] currentCommand, int index)
+{
+    return currentCommand[index];
+}
+
+void AddUser(SortedDictionary<string, SortedSet<string>> sides, string side, string user)
+{
+    if (!sides.ContainsKey(side))
+    {
+        sides.Add(side, new SortedSet<string>());
+    }
+    sides[side].Add(user);
+
+}
---
+++ SetsAndDictionariesAdvanced/SetsAndDictionariesAdvanced-Exercise/9. ExResults/Program.cs
@@ -0,0 +1,59 @@
+﻿SortedDictionary<string, int> students = new SortedDictionary<string, int>();
+SortedDictionary<string, int> submissions = new SortedDictionary<string, int>();
+
+string command = Console.ReadLine();
+
+while (command != "exam finished")
+{
+    string[] currentCommand = command.Split('-', StringSplitOptions.RemoveEmptyEntries);
+
+    string username = currentCommand[0];
+    string language = currentCommand[1];
+
+    if (language == "banned")
+    {
+        if (students.ContainsKey(username))
+        {
+            students.Remove(username);
+        }
+    }
+    else
+    {
+        int points = int.Parse(currentCommand[2]);
+
+        if (!students.ContainsKey(username))
+        {
+            students.Add(username, points);
+        }
+        else
+        {
+            if (points > students[username])
+            {
+                students[username] = points;
+            }
+        }
+
+        if (!submissions.ContainsKey(language))
+        {
+            submissions.Add(language, 1);
+        }
+        else
+        {
+            submissions[language]++;
+        }
+    }
+    command = Console.ReadLine();
+}
+
+Console.WriteLine("Results:");
+foreach (var student in students.OrderByDescending(x => x.Value))
+{
+    Console.WriteLine($"{student.Key} | {student.Value}");
+}
+
+Console.WriteLine("Submissions:");
+
+foreach (var contest in submissions.OrderByDescending(x => x.Value))
+{
+    Console.WriteLine($"{contest.Key} - {contest.Value}");
+}
