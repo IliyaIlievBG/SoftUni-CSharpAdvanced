680d6df9be6d785fa77f427a9692b772d78a732d
Iliya Iliev <iliya.ilievbg@gmail.com>
2024-03-24 01:23:01 +0200

    Adding tasks
    Adding first 3 tasks

--- Streams, Files and Directories/Streams, Files and Directories Lab/1. Odd Lines/Program.cs
+++ Streams, Files and Directories/Streams, Files and Directories Lab/1. Odd Lines/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace OddLines
+{
+    public class OddLines
+    {
+        static void Main()
+        {
+            string inputFilePath = @"..\..\..\Files\input.txt";
+            string outputFilePath = @"..\..\..\Files\output.txt";
+
+            ExtractOddLines(inputFilePath, outputFilePath);
+        }
+
+        public static void ExtractOddLines(string inputFilePath, string outputFilePath)
+        {
+            using (StreamReader reader = new StreamReader(inputFilePath))
+            {
+                using (StreamWriter writer = new StreamWriter(outputFilePath))
+                {
+                    int lineNumber = 0;
+                    while (!reader.EndOfStream)
+                    {
+                        lineNumber++;
+                        string currentLine = reader.ReadLine();
+                        if (lineNumber % 2 == 1)
+                        {
+                            continue;
+                        }
+                        
+                        writer.WriteLine(currentLine);
+                    }
+                }
+            }
+        }
+    }
+}
