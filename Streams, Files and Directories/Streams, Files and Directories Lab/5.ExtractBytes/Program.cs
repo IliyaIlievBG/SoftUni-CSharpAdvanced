aba4f6fc8ea5ea92d089d96af82d25cddb6b6b6b
Iliya Iliev <iliya.ilievbg@gmail.com>
2024-03-25 00:20:33 +0200

    adding tasks

---
+++ Streams, Files and Directories/Streams, Files and Directories Lab/5.ExtractBytes/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace ExtractSpecialBytes
+{
+    using System.Text;
+    public class ExtractSpecialBytes
+    {
+        static void Main()
+        {
+            string binaryFilePath = @"..\..\..\Files\example.png";
+            string bytesFilePath = @"..\..\..\Files\bytes.txt";
+            string outputPath = @"..\..\..\Files\output.bin";
+
+            ExtractBytesFromBinaryFile(binaryFilePath, bytesFilePath, outputPath);
+        }
+
+        public static void ExtractBytesFromBinaryFile(string binaryFilePath, string bytesFilePath, string outputPath)
+        {
+            using (FileStream myWriter = new FileStream(outputPath, FileMode.Open))
+            {
+                HashSet<byte> bytes = new HashSet<byte>();
+
+                using (StreamReader myReader = new StreamReader(bytesFilePath))
+                {
+                    bytes = Encoding.UTF8.GetBytes(bytesFilePath).ToHashSet();
+                }
+
+                using (FileStream myReader = new FileStream(binaryFilePath, FileMode.Open))
+                {
+                    byte[] bytesArray = new byte[1];
+
+                    int currentByte;
+                    while ((currentByte = myReader.Read(bytesArray, 0, bytesArray.Length)) != 0)
+                    {
+                        if (bytes.Contains(bytesArray[0]))
+                        {
+                            myWriter.Write(bytesArray, 0, bytesArray.Length);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
