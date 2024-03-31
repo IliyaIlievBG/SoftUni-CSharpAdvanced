9d4f235de2e454e671347f6fb98c994a6713eb23
Iliya Iliev <iliya.ilievbg@gmail.com>
2024-04-01 01:39:02 +0300

    Initial setup + first task

---
+++ Streams, Files and Directories/Streams Files and Directories - Exercise/1.EvenLines/Program.cs
@@ -0,0 +1,49 @@
+﻿using System.Text;
+
+namespace EvenLines
+{
+    using System;
+
+    public class EvenLines
+    {
+        static void Main()
+        {
+            string inputFilePath = @"..\..\..\text.txt";
+
+            Console.WriteLine(ProcessLines(inputFilePath));
+        }
+
+        public static string ProcessLines(string inputFilePath)
+        {
+            StringBuilder output = new StringBuilder();
+
+            using (StreamReader myReader = new StreamReader(inputFilePath))
+            {
+                int lineNumbr = 0;
+                char[] charsToReplace = { '-', ',', '.', '!', '?' };
+
+                while (!myReader.EndOfStream)
+                {
+                    string currentLine = myReader.ReadLine();
+                    string[] wordsInCurrentLine = currentLine.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                    if (lineNumbr % 2 == 0)
+                    {
+                        for (int i = wordsInCurrentLine.Length - 1; i >= 0; i--)
+                        {
+                            string currentWord = new string(wordsInCurrentLine[i]
+                                .Select(c => charsToReplace.Contains(c) ? '@' : c).ToArray());
+
+                            output.Append($"{currentWord} ");
+                        }
+                    }
+
+                    lineNumbr++;
+                }
+
+            }
+
+            return output.ToString().TrimEnd();
+        }
+    }
+}
