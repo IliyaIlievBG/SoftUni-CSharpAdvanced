48eb8398f10937f0e3df342f1e6a40ee023d0910
Iliya Iliev <iliya.ilievbg@gmail.com>
2024-04-28 18:38:29 +0300

    adding problems

--- StreamsFilesAndDirectories/StreamsFilesAndDirectories-Exrc/2.LineNumbers/Program.cs
+++ StreamsFilesAndDirectories/StreamsFilesAndDirectories-Exrc/2.LineNumbers/Program.cs
@@ -7,10 +7,22 @@
         {
             string inputFilePath = @"..\..\..\text.txt";
             string outputFilePath = @"..\..\..\newFile.txt";
+
+            ProcessLines(inputFilePath, outputFilePath);
         }
         public static void ProcessLines(string inputFilePath, string outputFilePath)
         {
+            string[] inputFileLines = File.ReadAllLines(inputFilePath);
 
+            for (int i = 0; i < inputFileLines.Length; i++)
+            {
+                int lettersCount = inputFileLines[i].Where(char.IsLetter).Count();
+                int punctuationMarksCount = inputFileLines[i].Where(char.IsPunctuation).Count();
+
+                inputFileLines[i] = $"Line {i + 1}: {inputFileLines[i]} ({lettersCount})({punctuationMarksCount})";
+            }
+
+            File.WriteAllLines(outputFilePath, inputFileLines);
         }
     }
 }
