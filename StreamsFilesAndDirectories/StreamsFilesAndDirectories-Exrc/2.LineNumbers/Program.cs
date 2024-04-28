475b33ab8cf3a114e25598b3331249faaab290b6
Iliya Iliev <iliya.ilievbg@gmail.com>
2024-04-28 17:07:35 +0300

    update

---
+++ StreamsFilesAndDirectories/StreamsFilesAndDirectories-Exrc/2.LineNumbers/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace LineNumbers
+{
+    using System;
+    public class LineNumbers
+    {
+        static void Main()
+        {
+            string inputFilePath = @"..\..\..\text.txt";
+            string outputFilePath = @"..\..\..\newFile.txt";
+        }
+        public static void ProcessLines(string inputFilePath, string outputFilePath)
+        {
+
+        }
+    }
+}
+
